9ec4419426818136ab8c423e26feefec80e143e0
James Aworo <james.aworo@outlook.com>
2023-06-30 08:47:02 +0100

    (api): add main application class

---
+++ medpay-api/src/main/java/com/james/medpay/MedPayApplication.java
@@ -0,0 +1,14 @@
+package com.james.medpay;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class MedPayApplication {
+
+	public static void main( String[] args ) {
+		run( MedPayApplication.class, args );
+	}
+
+}
