3cab6f84eb34db162c7c21ec59d4675359cb7e65
James Aworo <james.aworo@outlook.com>
2023-07-01 15:57:59 +0100

    update(api-authentication): add route to handle mobile users registration

--- medpay-api/src/main/java/com/james/medpay/features/authentication/data/request/RegisterRequest.java
+++ medpay-api/src/main/java/com/james/medpay/features/authentication/data/request/RegisterRequest.java
@@ -1,16 +1,14 @@
 package com.james.medpay.features.authentication.data.request;
 
+import com.james.medpay.features.users.domain.entity.enums.UserTypeEnum;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
 public class RegisterRequest {
-	@NotNull( message = "Username or email cannot be null" )
-	@NotEmpty( message = "Username or email cannot be empty" )
-	private String username;
-
 	@NotNull( message = "Password cannot be null" )
 	@NotEmpty( message = "Password cannot be empty" )
 	private String password;
@@ -19,8 +17,19 @@ public class RegisterRequest {
 	String name;
 
 	@NotNull(message = "Phone number is required")
-	String phoneNumber;
+	String phone;
 
-	@NotNull(message = "Email address is required")
+	@NotNull(message = "Email is required")
+	@Email(message = "Email should be a valid email address")
 	String email;
+
+	private Long id;
+	private String uuid;
+	private String username;
+	private Boolean isEnabled;
+	private UserTypeEnum type;
+	private String token;
+	private String nickname;
+
+
 }
